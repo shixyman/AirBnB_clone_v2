82508428569a9cd31072a96d27f2c06fbfffce52
Abdeljalil37 <widadpop@gmail.com>
2023-12-20 15:49:57 +0100

    recreate the function reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
@@ -41,26 +35,33 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        """ delete an existing element """
+        ''' delete obj from __objects if it's inside '''
+
         if obj:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
+            key = '{}.{}'.format(type(obj).__name__, obj.id)
             if key in self.__objects:
                 del self.__objects[key]
                 self.save()
-
