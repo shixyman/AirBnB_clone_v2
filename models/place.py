e18a8017b0f28f98aae643d699e548f665bcccfe
Abdeljalil37 <widadpop@gmail.com>
2023-12-20 15:37:37 +0100

    update place model

--- models/place.py
+++ models/place.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+"""This is the place class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 import models
+from models.review import Review
 
 
 place_amenity = Table("place_amenity", Base.metadata,
-                      Column("place_id", String(60),
-                             ForeignKey("places.id"),
-                             primary_key=True,
-                             nullable=False),
-                      Column("amenity_id", String(60),
-                             ForeignKey("amenities.id"),
-                             primary_key=True,
-                             nullable=False))
+                      Column("place_id", String(60),ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column("amenity_id", String(60),ForeignKey("amenities.id"),
+                             primary_key=True,nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -46,3 +43,30 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     amenity_ids = []
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                               backref="place")
+        amenities = relationship("Amenity", secondary=place_amenity,
+                            viewonly=False,
+                            back_populates="place_amenities")
+    
+    else:
+        @property
+        def reviews(self):
+            """getter attribute returns the list of Review instances"""
+            lista = []
+            for key in models.storage.all(Review).values():
+                if key.place_id == self.id:
+                    lista.append(key)
+            return lista
+        
+        @property
+        def amenities(self):
+            """ Returns list of amenity ids """
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            """ Appends amenity ids to the attribute """
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
