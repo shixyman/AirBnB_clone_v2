00b3264f00c9c600a0236164be94fe867e2d4f7c
Abdeljalil37 <widadpop@gmail.com>
2023-12-20 15:40:37 +0100

    fix save function

--- models/base_model.py
+++ models/base_model.py
@@ -54,10 +54,6 @@ class BaseModel:
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
-    def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         """creates dictionary of the class  and returns
