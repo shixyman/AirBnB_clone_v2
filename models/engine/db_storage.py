7bbfaa66074175d1dfff8c2cde09e64779cdc95d
Abdeljalil37 <widadpop@gmail.com>
2023-12-20 15:51:16 +0100

    db storage init fucntion

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -6,9 +6,24 @@ from models.base_model import Base
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import (create_engine)
 from os import getenv
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 
 class DBStorage:
     """ create tables in environmental"""
     __engine = None
     __session = None
+
+    def __init__(self):
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(getenv("HBNB_MYSQL_USER"), getenv("HBNB_MYSQL_PWD"),
+                                              getenv("HBNB_MYSQL_HOST"), getenv("HBNB_MYSQL_DB")),
+                                      pool_pre_ping=True)
+
+        if getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
