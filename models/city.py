4b8b5cfb14817e602068ae25cbe1ebc6aae3b147
Abdeljalil37 <widadpop@gmail.com>
2023-12-20 13:51:31 +0100

    city declaration error fixed

--- models/city.py
+++ models/city.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 
 class City(BaseModel, Base):
     """This is the class for City
